e09e337af9efbd72bf59192994b6aecff3bc5a88
OswaldoRo <116927565+OswaldoRo@users.noreply.github.com>
2026-02-22 22:42:49 -0500

    Agregamos paquete borrador para practicar

--- src/borrador/UsuarioServiceb.java
+++ src/borrador/UsuarioServiceb.java
@@ -1,4 +1,41 @@
 package borrador;
 
-public class UsuarioServiceb {
+import java.util.ArrayList;
+public class UsuarioServiceb{
+
+    ArrayList<Usuariob>listaUsuariosb = new ArrayList<>();
+
+    //Agregar usuarios al array
+
+    public void agregarUsuariosb(Usuariob usuariob){
+        listaUsuariosb.add(usuariob);
+    }
+
+    // Leer o buscar por id
+
+    public Usuariob buscarId(int id){
+        for(int i = 0;i < listaUsuariosb.size();i++){
+            if(listaUsuariosb.get(i).getId() == id){
+                return listaUsuariosb.get(i);
+            }
+        }
+        return null;
+    }
+
+    // Devuelve la lista completa de usuarios
+
+
+
+
+
+
+
+
+
+
+
+    public ArrayList<Usuariob> mostrarLista(){
+        return listaUsuarios;
+    }
 }
+
